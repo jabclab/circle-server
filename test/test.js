2e555ce72d176c2d815e91df983269142c86dbfc
jabclab <jake.clarkson@digitalanimal.com>
2013-03-12 16:34:52 +0000

    Resolving tests using dependency injection

--- test/test.js
+++ test/test.js
@@ -1,6 +1,7 @@
 var person = new require('../lib/core').Person;
-
-const errRegEx = /^(\w|\s)+$/;
+	rewire = require('rewire'),
+	rw_core = rewire('../lib/core'),
+	errRegEx = /^(\w|\s)+$/;
 
 describe('Person', function() {
 	describe('#getName()', function() {
@@ -37,6 +38,53 @@ describe('Person', function() {
 				callback(err, records);
 				done();
 				callback.should.have.been.calledWith(null);
+			});
+		});
+		it('should call callback with an error if opening Mongo client fails', function(done) {
+			var callback = sinon.spy();
+
+			// Mock the `client` variable in the core.js file to call the callback passed to it with
+			// a not null error specified when the `open` method is called on it.
+			rw_core.__set__('client', {
+				open: function(callback) {
+					callback('error');
+				}
+			});
+
+			Object.create(rw_core.Person).save({ name: 'Jake' }, function(err, records) {
+				callback(err, records);
+				done();
+				callback.should.have.been.calledWithMatch(errRegEx);
+			});
+		});
+		it('should call callback with an error if inserting document fails', function(done) {
+			var callback = sinon.spy();
+
+			rw_core.__set__('client', {
+				open: function(callback) {
+					// The `open` method should work
+					callback(null, {
+						db: function(name) {
+							return {
+								collection: function(name) {
+									return {
+										insert: function(obj, callback) {
+											callback('error');
+										}
+									}
+								}
+							}
+						},
+						close: function() {
+						}	
+					});
+				}
+			});
+
+			Object.create(rw_core.Person).save({ name: 'Jake' }, function(err, records) {
+				callback(err, records);
+				done();
+				callback.should.have.been.calledWithMatch(errRegEx);
 			});
 		});
 	});
