a4e73622c9b994cae6b22900d63ad6fdbd81b00e
jabclab <jake.clarkson@digitalanimal.com>
2013-03-13 10:00:40 +0000

    Use of sinon stubs

--- test/test.js
+++ test/test.js
@@ -1,22 +1,18 @@
-var person = new require('../lib/core').Person;
+var person = require('../lib/core').Person;
 	rewire = require('rewire'),
-	rw_core = rewire('../lib/core'),
-	errRegEx = /^(\w|\s)+$/;
+	errRegEx = /^(\w|\s)+$/,
+	data = {
+		validPerson: { name: 'Jake' }
+	};
 
 describe('Person', function() {
-	describe('#getName()', function() {
-		it('should return "Jake" if odd number passed in', function() {
-			person.getName(3).should.equal('Jake');
-			person.getName(9).should.equal('Jake');
-			person.getName(3291).should.equal('Jake');
-		});
-		it('should return "John" if odd number passed in', function() {
-			person.getName(0).should.equal('John');
-			person.getName(2).should.equal('John');
-			person.getName(122).should.equal('John');
-		});
-	});
 	describe('#save()', function() {
+		var rewiredCore = {};
+
+		beforeEach(function() {
+			rewiredCore = rewire('../lib/core');
+		});
+
 		it('should throw an error if specified person to save is not an object', function() {
 			var callback = sinon.spy();
 
@@ -34,54 +30,54 @@ describe('Person', function() {
 		it('should not throw an error if object is passed and contains a `name` property', function(done) {
 			var callback = sinon.spy();
 
-			person.save({ name: 'Jake Clarkson' }, function(err, records) {
+			person.save(data.validPerson, function(err, records) {
 				callback(err, records);
 				done();
 				callback.should.have.been.calledWith(null);
 			});
 		});
 		it('should call callback with an error if opening Mongo client fails', function(done) {
-			var callback = sinon.spy();
+			var callback = sinon.spy(),
+				client = rewiredCore.__get__('client');
 
-			// Mock the `client` variable in the core.js file to call the callback passed to it with
-			// a not null error specified when the `open` method is called on it.
-			rw_core.__set__('client', {
-				open: function(callback) {
-					callback('error');
-				}
+			sinon.stub(client, 'open', function(callback) {
+				callback('error');
 			});
 
-			Object.create(rw_core.Person).save({ name: 'Jake' }, function(err, records) {
+			rewiredCore.__set__('client', client);
+
+			rewiredCore.Person.save(data.validPerson, function(err, records) {
 				callback(err, records);
 				done();
 				callback.should.have.been.calledWithMatch(errRegEx);
 			});
 		});
 		it('should call callback with an error if inserting document fails', function(done) {
-			var callback = sinon.spy();
-
-			rw_core.__set__('client', {
-				open: function(callback) {
-					// The `open` method should work
-					callback(null, {
-						db: function(name) {
-							return {
-								collection: function(name) {
-									return {
-										insert: function(obj, callback) {
-											callback('error');
-										}
+			var callback = sinon.spy(),
+				client = rewiredCore.__get__('client');
+			
+			// Stub the `open` method of the `client` `Object`
+			sinon.stub(client, 'open', function(callback) {
+				callback(null, {
+					db: function(name) {
+						return {
+							collection: function(name) {
+								return {
+									insert: function(obj, callback) {
+										callback('error');
 									}
 								}
 							}
-						},
-						close: function() {
-						}	
-					});
-				}
+						}
+					},
+					close: function() {
+					}	
+				});
 			});
 
-			Object.create(rw_core.Person).save({ name: 'Jake' }, function(err, records) {
+			rewiredCore.__set__('client', client);
+
+			rewiredCore.Person.save(data.validPerson, function(err, records) {
 				callback(err, records);
 				done();
 				callback.should.have.been.calledWithMatch(errRegEx);
